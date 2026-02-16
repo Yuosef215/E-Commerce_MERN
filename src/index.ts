1e6b05d573774173dac9e768f520110805212b44
Yousef Alaa <youssefpoofon46@gmail.com>
2026-02-17 00:25:42 +0200

    Product Feature

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import userRout from "./routes/userRout.js";
+import { seedInitialProducts } from "./services/productServices.js";
+import productRoute from "./routes/productRoute.js";
 
 const app = express();
 const port = 3001;
@@ -13,7 +15,10 @@ mongoose.connect("mongodb://127.0.0.1:27017/ecommerce").then(() => {
   console.error("Failed to connect to MongoDB", err);
 });
 
+seedInitialProducts();
+
 app.use('/user',userRout )
+app.use('/product', productRoute)
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
